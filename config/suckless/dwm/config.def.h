4af3bf9bba78a6ee32c4eb1f9ddf0bc0872af318
francescovolpini <81026654+francescovolpini@users.noreply.github.com>
2021-08-25 14:47:33 +0200

    Update config.def.h

--- config/suckless/dwm/config.def.h
+++ config/suckless/dwm/config.def.h
@@ -12,7 +12,8 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "monospace:size=10" };
+static char *fonts[]     = {"Mononoki Nerd Font:size=9:antialias=true:autohint=true", "Twemoji:size=10:antialias=true:autohint=true", "JoyPixels:size=10:antialias=true:autohint=true" };
+//static const char *fonts[]     = {"Mononoki Nerd Font:size=9:antialias=true:autohint=true", "Twemoji:size=10:antialias=true:autohint=true", "JoyPixels:size=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
